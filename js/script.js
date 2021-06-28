bebea186f5af81c275895903109b5bd4bd49a379
kuya-ui <kuyamaxmillan@gmail.com>
2021-06-28 23:52:55 +0300

    add link

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function max() {
+    let control = document.getElementById('controls').value;
+    if (control == "") {
+        alert("invalid response");
+    } else {
+        alert("We have received your message. Thank you fro reaching out to us")
+    };
+};
