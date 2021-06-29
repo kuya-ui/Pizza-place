c847223be67de9be2665ce28333899e9cd8c90ec
kuya-ui <kuyamaxmillan@gmail.com>
2021-06-29 21:31:14 +0300

    add js

--- js/script.js
+++ js/script.js
@@ -1,45 +1,80 @@
-// function getSizeValue() {
-//     var selectedValue = document.getElementById("size").value;
-//     return parseInt(selectedValue);
-//     console.log(size)
-// }
-// function getCrust() {
-//     var selectedValue = document.getElementById("crust").value;
-//     return parseInt(selectedCrust);
-// }
-// function getToppings() {
-//     var selectedValue = document.getElementById("toppings").value;
-//     return parseInt(selectedTopppings);
-// }
-// function getSizeFlavor() {
-//     var selectedValue = document.getElementById("flavor").value;
-//     return parseInt(selectedFlavor);
-// }
-// function getOrder(){
-//     var selectedOrder = getElementById("order1").value;
-//     return parseInt(selectedOrder);
-// }
-// function getTotalAmount(){
-//     var totalAmount = (getSizeValue() + getCrust() + getToppings())*getTotal
-//     a
-// }
-// function max() {
-//     let control = document.getElementById('controls').value;
-//     if (control == "") {
-//         alert("invalid response");
-//     } else {
-//         alert("We have received your message. Thank you fro reaching out to us")
-//     };
-// };
-var price,crust,toppings,total,charge;
-var delivery;
-function Pizza(flavor,crust,size,toppings,orders){
-    this.Flavor = flavor;
-    this.Crust = crust;
-    this.Size = size;
-    this.Toppings = toppings;
-    this.Orders = orders;
+function Pizza(flavors,crust,size,toppings,order){
+    this.flavors = flavors;
+    this.crust = crust;
+    this.size = size;
+    this.toppings = toppings;
+    this.order = order;
     console.log(flavor,crust,size,toppings,orders);
 
 }
-var star = new Pizza(300,250,550,200,300)
+$(document).ready(function(){
+    $("#order").click(function(event){
+        let pizza_flavor = $("#flavor option:selected").val();
+        let moon_size = $("#size option:selected").val();
+        let star_crust = $("#crust option:selected").val();
+        let sun_toppings = $("#toppings option:selected").val();
+
+        switch(pizz_flavor){
+            case "0":
+                price =0;
+            break;
+            case "pepperoni"
+                price = 300;
+                break;
+                case "pineapple"
+                price = 250;
+                break;
+                case = "chicken"
+                price =250;
+                break;
+                case = "beef"
+                price = 200;
+                default:
+            }
+        switch(moon_size){
+            case "0":
+              price =0;
+            break;
+            case "large":
+               price = 1200;
+             break;
+             case "medium":
+               price = 800;
+             break;
+             case "regular":
+               price = 500;
+             default:
+           }
+           switch(star_crust){
+               case "0":
+                   price =0;
+                break
+                case "crispy"
+                    price = 150;
+                break;
+                case "stuffed"
+                    price = 100;
+                break;
+                case "gluten-free"
+                price = 100;
+                default:
+           }
+           switch(sun_toppings){
+            case "0":
+              price =0;
+            break;
+            case "mushroom":
+               price = 200;
+             break;
+             case "avocado":
+               price = 50;
+             break;
+             case "italian":
+               price = 250;
+             break:
+              case "mexican":
+                price = 250;
+              default:
+           }
+    });
+});
