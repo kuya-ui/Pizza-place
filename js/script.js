e6bdd21f607b3f61bb7f6a8fa29078ceb4f706c4
kuya-ui <kuyamaxmillan@gmail.com>
2021-06-30 08:27:51 +0300

    change

--- js/script.js
+++ js/script.js
@@ -14,67 +14,83 @@ $(document).ready(function(){
         let star_crust = $("#crust option:selected").val();
         let sun_toppings = $("#toppings option:selected").val();
 
-        switch(pizz_flavor){
+        switch(pizza_flavor){
             case "0":
-                price =0;
+                flavor_price =0;
             break;
-            case "pepperoni"
-                price = 300;
+            case "pepperoni":
+                flavor_price = 300;
+            break;
+                case "pine apple":
+                flavor_price = 250;
                 break;
-                case "pineapple"
-                price = 250;
+                case "chicken":
+                flavor_price =250;
                 break;
-                case = "chicken"
-                price =250;
-                break;
-                case = "beef"
-                price = 200;
+                case "beef":
+               flavor_price = 200;
                 default:
             }
         switch(moon_size){
             case "0":
-              price =0;
+              moon_price =0;
             break;
             case "large":
-               price = 1200;
+               moon_price = 1200;
              break;
              case "medium":
-               price = 800;
+               moon_price = 800;
              break;
              case "regular":
-               price = 500;
+               moon_price = 500;
              default:
            }
            switch(star_crust){
-               case "0":
-                   price =0;
-                break
-                case "crispy"
-                    price = 150;
-                break;
-                case "stuffed"
-                    price = 100;
-                break;
-                case "gluten-free"
-                price = 100;
-                default:
+            case "0":
+              crust_price = 0;
+            break;
+            case "crispy":
+              crust_price = 150;
+             break;
+             case "stuffed":
+               crust_price = 100;
+             break;
+             case "gluten-free":
+               crust_price = 100;
+             default:
            }
            switch(sun_toppings){
             case "0":
-              price =0;
+              toppings_price =0;
             break;
             case "mushroom":
-               price = 200;
+               toppings_price = 200;
              break;
              case "avocado":
-               price = 50;
+               toppings_price = 50;
              break;
              case "italian":
-               price = 250;
-             break:
+                toppings_price = 250;
+             break;
               case "mexican":
-                price = 250;
+                toppings_price = 250;
               default:
            }
+           if((pizza_flavor == "0") && (moon_size == "0") && (star_crust == "0") && (sun_toppings == "0")){
+            console.log("nothing selected");
+            $("#order").show();
+            $("#pink").hide();
+            alert("Please select pizza size and crust");
+          }
+          else{
+            $("#order").hide();
+            $("#pink").fadeIn(2000);
+          }
+          var total = flavor_prise + moon_price  + flavor_price + toppings_price;
+           $("#blue").html($("#toppings option:selected").val());
+           $("#red").html( $("#size option:selected").val());
+           $("#black").html($("#crust option:selected").val());
+           $("#green").html($("#flavors option:selected").val());
+           $("#son").html(total);
     });
 });
